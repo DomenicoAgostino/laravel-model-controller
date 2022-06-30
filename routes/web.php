294aa0cfd93ef5255871284f1b6947e288aa1cac
DomenicoAgostino <99025293+DomenicoAgostino@users.noreply.github.com>
2022-06-30 16:01:39 +0200

    PageController Preparato

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PageController@index')->name('home');
+
+Route::get('/about', 'PageController@index')->name('about');
+
+Route::get('/', 'PageController@index')->name('contacts');
+
+Route::get('/', 'PageController@index')->name('movie');
+
+
+
+
+
+
